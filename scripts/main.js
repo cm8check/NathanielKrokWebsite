4e903514001b9416ff19b0925d5b521502c51489
cm8check <hammerman1010@gmail.com>
2017-12-26 23:51:15 -0600

    Commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var sideMenu = document.getElementById("side-menu");
+
+function openSlideMenu() {
+    sideMenu.style.width = "200px";
+}
+
+function closeSlideMenu() {
+    sideMenu.style.width = "0px";
+}
